a8450520137f8abb38e8a88e40df9bd71568a215
Syed Muhammad Haris Shah <engrharisshah1122@gmail.com>
2025-10-16 15:14:14 +0500

    alaka ma hom d feature bandi kar oko

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,7 @@ const User = require("../models/User");
 // 🟢 Signup
 //home page
 //login page
+// da nsadjkhfhjkdsahfjldshfvdsajlh
 exports.signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
